ca362771d52de60ebe56f88f9fa866e4bc59bd42
Juan Camilo Salazar <juancspace@outlook.com>
2022-08-01 23:57:04 -0500

    The first version of the header is ready.

---
+++ themes/myspace/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function init_template() {
+
+	add_theme_support('post-thumbnails');
+	add_theme_support('title-tag');
+
+	register_nav_menus(
+		array(
+			'top_menu' => 'Main Menu'
+		)
+	);
+}
+
+add_action('after_setup_theme', 'init_template');
+
+function assets() {
+	wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css', '', '5.2.0', 'all');
+	wp_register_style( 'ubuntu', 'https://fonts.googleapis.com/css2?family=Ubuntu:wght@500;700&display=swap', '', '1.0', 'all');
+	wp_register_style( 'sourcesanspro', 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;1,300&display=swap', '', '1.0', 'all');
+
+	wp_enqueue_style( 'estilos', get_stylesheet_uri(), array('bootstrap', 'ubuntu', 'sourcesanspro'), '1.0', 'all');
+
+	wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js', '', '2.11.5', true);
+	wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js', array('jquery', 'pooper'), '5.2.0', true);
+
+	wp_enqueue_script('custom', get_template_directory_uri().'/assets/js/index.js', '', '1.0', true);
+}
+
+add_action( 'wp_enqueue_scripts', 'assets');
--- themes/myspace/header.php
+++ themes/myspace/header.php
@@ -4,7 +4,31 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php wp_head() ?>
+	<?php wp_head(); ?>
 </head>
 <body>
 
+<header class="header">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark header__navbar">
+		<div class="container align-items-center">
+			<img src="<?php echo get_template_directory_uri()?>/assets/img/logo-header2.png" alt="logo">
+			<div class="d-flex pe-3 pt-3">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<nav>
+						<?php wp_nav_menu(
+							array(
+								'theme_location' => 'top_menu',
+								'menu_class' => 'container__menu__wp-navbar',
+								'container_class' => 'container__menu',
+							)
+						);?>
+					</nav>
+				</div>
+			</div>
+
+		</div>
+	</nav>
+</header>
