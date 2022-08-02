a7b5997473c8f30109bb76e39c6a68808e84fea7
Juan Camilo Salazar <juancspace@outlook.com>
2022-08-02 09:57:27 -0500

    The first version of the footer is completed.

--- themes/myspace/footer.php
+++ themes/myspace/footer.php
@@ -1,3 +1,23 @@
+<footer class="footer">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-3 footer__logo">
+				<img src="<?php echo get_template_directory_uri()?>/assets/img/footer-logo2.png" alt="logo" height="80px">
+			</div>
+			<div class="col-4 offset-2 pt-3">
+				<p class="footer__text-made">Made by 🛠️ Juan Camilo</p>
+			</div>
+			<div class="col-3 pt-3 footer__projects">
+				<p>Outstanding projects</p>
+				<ul class="d-flex footer__projects--links">
+					<li><a href="#">Project 1</a></li>
+					<li><a href="#">Project 2</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
